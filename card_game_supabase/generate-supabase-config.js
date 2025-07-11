49cf9ac967ea3f6bc66c24b4cd1a56b96b718e07
canine89 <phk707kr@gmail.com>
2025-07-11 09:59:34 +0900

    supabase-config.js 생성 경로 수정 (Vercel 빌드 오류 해결)

--- card_game_supabase/generate-supabase-config.js
+++ card_game_supabase/generate-supabase-config.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 const config = `\nwindow.SUPABASE_URL = '${process.env.SUPABASE_URL}';\nwindow.SUPABASE_ANON_KEY = '${process.env.SUPABASE_ANON_KEY}';\n`;
-fs.writeFileSync('card_game_supabase/supabase-config.js', config); 
+fs.writeFileSync('supabase-config.js', config); 
